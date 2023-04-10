11794ec879bd0c898b24354b97cab4617742e823
Sticky <adrian.gutierrez@techsoftconsulting.mx>
2023-04-09 23:30:26 -0700

    added new fields

--- src/components/PromptForm/index.jsx
+++ src/components/PromptForm/index.jsx
@@ -10,8 +10,21 @@ import Button from '@mui/material/Button';
 export const PromptForm = ({showBot}) => { 
     const [botGoal, setBotGoal] = useState("");
     const [telegramApiKey, setTelegramApiKey] = useState("");
+    const [userContent1, setUserContent1] = useState("");
+    const [userContent2, setUserContent2] = useState("");
+    const [userContent3, setUserContent3] = useState("");
+    const [assistantContent1, setAssistantContent1] = useState("");
+    const [assistantContent2, setAssistantContent2] = useState("");
+    const [assistantContent3, setAssistantContent3] = useState("");
+
     const changeBotGoal = (e) => setBotGoal(e.target.value);
     const changeApiKey = (e) => setTelegramApiKey(e.target.value);
+    const changeUserContent1 = (e) => setUserContent1(e.target.value);
+    const changeUserContent2 = (e) => setUserContent2(e.target.value);
+    const changeUserContent3 = (e) => setUserContent3(e.target.value);
+    const changeAssistantContent1 = (e) => setAssistantContent1(e.target.value);
+    const changeAssistantContent2 = (e) => setAssistantContent2(e.target.value);
+    const changeAssistantContent3 = (e) => setAssistantContent3(e.target.value);
 
 
     const handleSendButton = () => {
@@ -22,7 +35,13 @@ export const PromptForm = ({showBot}) => {
           },
           body: JSON.stringify({
             botGoal: botGoal,
-            telegramApiKey: telegramApiKey
+            telegramApiKey: telegramApiKey,
+            userContent1: userContent1,
+            userContent2: userContent2,
+            userContent3: userContent3,
+            assistantContent1: assistantContent1, 
+            assistantContent2: assistantContent2,
+            assistantContent3: assistantContent3
           })
         })
         .then(res => res.json())
@@ -72,6 +91,60 @@ export const PromptForm = ({showBot}) => {
             type="text"
             value={telegramApiKey}
             onChange={changeApiKey}
+            multiline
+            maxRows={4}
+        />
+        <TextField
+            id="outlined-multiline-flexible"
+            label="Training user query 1"
+            type="text"
+            value={userContent1}
+            onChange={changeUserContent1}
+            multiline
+            maxRows={4}
+        />
+        <TextField
+            id="outlined-multiline-flexible"
+            label="Training agent response 1"
+            type="text"
+            value={assistantContent1}
+            onChange={changeAssistantContent1}
+            multiline
+            maxRows={4}
+        />
+        <TextField
+            id="outlined-multiline-flexible"
+            label="Training user query 2"
+            type="text"
+            value={userContent2}
+            onChange={changeUserContent2}
+            multiline
+            maxRows={4}
+        />
+        <TextField
+            id="outlined-multiline-flexible"
+            label="Training agent response 2"
+            type="text"
+            value={assistantContent2}
+            onChange={changeAssistantContent2}
+            multiline
+            maxRows={4}
+        />
+        <TextField
+            id="outlined-multiline-flexible"
+            label="Training user query 3"
+            type="text"
+            value={userContent3}
+            onChange={changeUserContent3}
+            multiline
+            maxRows={4}
+        />
+        <TextField
+            id="outlined-multiline-flexible"
+            label="Training agent response 3"
+            type="text"
+            value={assistantContent3}
+            onChange={changeAssistantContent3}
             multiline
             maxRows={4}
         />
