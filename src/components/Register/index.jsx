e4c327d50eab891575faf77dedb9cc6482e7973f
Sticky <adrian.gutierrez@techsoftconsulting.mx>
2023-05-14 21:17:30 -0600

    updated urls

--- src/components/Register/index.jsx
+++ src/components/Register/index.jsx
@@ -39,7 +39,7 @@ const Container = styled(Box)(({ theme }) => ({
     const registerGoogle = useGoogleLogin({
         onSuccess: tokenResponse => {
           const accessToken = tokenResponse.access_token;
-          fetch('http://localhost:3001/google-login', {
+          fetch('https://botpanelserver.adriangutierr26.repl.co/google-login', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
@@ -73,7 +73,7 @@ const Container = styled(Box)(({ theme }) => ({
 
       const handleRegister = async () => {
         try {
-          const response = await fetch('http://localhost:3001/set-password', {
+          const response = await fetch('https://botpanelserver.adriangutierr26.repl.co/set-password', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
