7177e4b9f0dc7ad2ab975409f81dc537c20af88c
Sticky <adrian.gutierrez@techsoftconsulting.mx>
2023-05-18 16:37:00 -0600

    ready

--- src/App.jsx
+++ src/App.jsx
@@ -5,38 +5,48 @@ import { PromptForm } from './components/PromptForm';
 import {React, useState} from 'react';
 import LoginComponent from './components/Login';
 import RegisterComponent from './components/Register';
-
 import { isMobileDevice } from './utils/checkdevice';
-
+import { Container, Box, Typography } from '@mui/material';
 
 function App() {
   const [botCreated, setBotCreated] = useState(false);
-  function showBot(){
-    setBotCreated(true)
+  function showBot() {
+    setBotCreated(true);
   }
-  
-  const mobileWarning = isMobileDevice() ? (
-    <div className="mobile-warning">
-    Please access using your computer/laptop
-  </div>
-  ) : null;
 
-return (
-  <div className="App">
-    {mobileWarning}
+  if (isMobileDevice()) {
+    return (
+      <Container maxWidth="sm" style={{ height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          flexDirection="column"
+          boxShadow={2}
+          borderRadius={4}
+          p={3}
+        >
+          <Typography variant="h5">Please access using a desktop computer or laptop</Typography>
+        </Box>
+      </Container>
+    );
+  }
+
+  return (
+    <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Mybots />}/>
-          <Route path="/login" element={<LoginComponent />}/>
-          <Route path="/my-bots" element={<Mybots />}/>
-          <Route path="/set-password" element={<RegisterComponent />}/>
-          <Route path="/create-bot" element={<PromptForm showBot={showBot}/>}/>
+          <Route path="/" element={<Mybots />} />
+          <Route path="/login" element={<LoginComponent />} />
+          <Route path="/my-bots" element={<Mybots />} />
+          <Route path="/set-password" element={<RegisterComponent />} />
+          <Route path="/create-bot" element={<PromptForm showBot={showBot} />} />
         </Routes>
       </Router>
-      
+
       {/*{botCreated && <p>Your own Gpt has been created!</p>}*/}
     </div>
-);
-
+  );
 }
+
 export default App;
