c78f485a6beb82b937647ce9b5bfb5e5c6ef99cb
OctaneIS <alexb@octane.com.mx>
2023-07-05 12:22:17 -0600

    nuevos archivos

---
+++ src/components/CreateBot/CallbackURL/index.jsx
@@ -0,0 +1,110 @@
+import {React, useState} from 'react';
+import { userSignTheme } from '../../../utils/userSignTheme';
+import useTextFieldData from '../../../hooks/useTextFieldData';
+import { Button, TextField, Box, ThemeProvider, Grid } from '@mui/material';
+import  Info from './assets/info.svg'
+
+const CallbackURL= ({handleCancel, handleContinue, handleBack, updateStepData, botId }) => {
+  const { textFieldValue1, textFieldValue2, handleTextField1Change, handleTextField2Change, isLoading } = useTextFieldData(botId, 9);
+  const [isValidStep, setIsValidStep] = useState(false);
+
+  const handleChange1 = (e) => {
+    handleTextField1Change(e);
+    updateStepData({ callbackURL: e.target.value });
+    if (e.target.value !== '') {
+      setIsValidStep(true);
+    } else {
+      setIsValidStep(false);
+    }
+  }
+
+ 
+
+    return (
+      <ThemeProvider theme={userSignTheme}>
+        <Grid container sx={{ minHeight: '100vh', background: 'linear-gradient(45deg, #6a1b9a 30%, #42a5f5 90%)' }}>
+          <Grid item xs={12}>
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                minHeight: '100vh',
+              }}
+            >
+
+              <Box
+                sx={{
+                  p: 4,
+                  borderRadius: '20px',
+                  bgcolor: 'white',
+                  position: 'relative',
+                  width: {
+                    xs: '80%', // en pantallas extra pequeñas (menos de 600px) el ancho será del 90%
+                    sm: '75%', // en pantallas pequeñas (600px o más) el ancho será del 75%
+                    md: '60%', // en pantallas medianas (960px o más) el ancho será del 60%
+                    lg: '50%', // en pantallas grandes (1280px o más) el ancho será del 50%
+                  },
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+              >
+                <Box
+                  sx={{ flex: '1', height: '100%' }}
+                >
+                  <Grid container sx={{display:'flex' }}>
+                      <Grid item xs sx={{display:'flex', alignSelf:'flex-start'}}>
+                      <Box my={1}>
+                          <Button variant='contained' color= 'error' onClick={handleCancel}>
+                          Cancel
+                          </Button>
+                      </Box>
+                      </Grid>
+                  </Grid>
+                  <p style={{display: 'flex', fontFamily:'poppins', fontSize:'1.2em', marginBottom: '0%', color:'#6F3FF8', fontWeight:'bold'}}>Whatsapp Callback URL</p>
+                  <TextField
+                    fullWidth
+                    id='whatsapp-callback'
+                    label='Whatsapp Callback URL'
+                    variant='outlined'
+                    placeholder={ textFieldValue1 ? '' : 'Whatsapp Callback URL'}
+                    value={ textFieldValue1 }
+                    onChange={handleChange1}
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                    sx={{ alignSelf: 'center', justifySelf: 'center', mb:'4%', mt:'4%'}}
+                  />
+                                    
+                  <div style={{display:"flex", flexDirection:'row', justifyContent:"center", alignContent:"center", justifyItems:"center", marginBottom:'3%'}}>
+                  <img src={Info} style={{marginRight:'2%'}}/>
+                  <a href='https://youtu.be/NUwN3exDJ6Y' target="_blank" ><p style={{display: 'flex', fontFamily:'inter', fontSize:'1em', color:'rgba(0, 0, 0, 0.5)', fontWeight:'bold'}}>Watch our video about how to get your Whatsapp Callback URL</p></a>
+                  </div>
+                 <Grid container sx={{display:'flex'}}>
+                    <Grid item xs sx={{display:'flex', flex: 0.5, justifyContent:'flex-start'}}>
+                      <Box my={1}>
+                        <Button variant='contained' color='backbutton' onClick={handleBack}>
+                          Go back
+                        </Button>
+                      </Box>
+                    </Grid>
+                      <Grid item xs sx={{display:'flex', flex: 0.5, justifyContent:'flex-end'}}>
+                      <Box my={1}>
+                        <Button variant='contained' color='success' onClick={handleContinue}disabled={!isValidStep}>
+                          Continue
+                        </Button>
+                      </Box>
+                    </Grid>
+                  </Grid>
+                </Box>
+              </Box>
+            </Box>
+          </Grid>
+        </Grid>
+      </ThemeProvider>
+    );
+  };
+
+  export default CallbackURL;
